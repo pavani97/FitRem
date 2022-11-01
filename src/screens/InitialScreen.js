d15f00ae85943e8cee1b725b55c1d6dd10771f6d
pavani97 <mvspavs@123>
2022-11-01 14:19:06 +0530

    firstcommit

---
+++ src/screens/InitialScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, StyeSheet } from "react-native-elements";
+
+const InitialScreen = () => {
+  return (
+    <>
+      <Text style={{ margin: 80, fontSize: 40 }}>
+        Welcome to remainder!!!!!
+      </Text>
+    </>
+  );
+};
+export default InitialScreen;
